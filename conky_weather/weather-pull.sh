a750af8a5d3f29112a4f10b699b4cacd5e86e4e5
Kosteron <account@linelo.fr>
2020-01-23 16:46:57 +0100

    Add weather alt

--- conky_weather/weather-pull.sh
+++ conky_weather/weather-pull.sh
@@ -2,7 +2,7 @@
 function weather-pull {
 	while true 
 	do
-		curl "http://wttr.in/aix-en-provence?T&1&Q&F&lang=fr" --silent --max-time 3 > .meteo
+		curl "http://wttr.in/aix-en-provence?T&Q&F&lang=fr" --silent --max-time 3 > /tmp/weather.tmp
 		sleep 600	
 	done
 }
