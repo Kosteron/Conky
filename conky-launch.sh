c953498fe8b34d8865f8ed07606774f7262e4fbb
Kosteron <account@linelo.fr>
2022-10-23 20:40:33 +0200

    Adding conky-rss-reader

--- conky-launch.sh
+++ conky-launch.sh
@@ -3,28 +3,32 @@
 killall conky
 
 # Start computer monitoring widget (based on Mainte conky script)
-conky -c $HOME/.conky/conky_computer_monitor/conkyrc-computer-metrics &
-sleep 0.8
-conky -c $HOME/.conky/conky_computer_monitor/conkyrc-exploded-view &
+#conky -c $HOME/.conky/conky_computer_monitor/conkyrc-computer-metrics &
+#sleep 0.8
+#conky -c $HOME/.conky/conky_computer_monitor/conkyrc-exploded-view &
 
 # start weather widget
-sleep 0.8
-bash $HOME/.conky/conky_weather/weather-pull.sh &
-sleep 1
-conky -c $HOME/.conky/conky_weather/conkyrc-weather &
+#sleep 0.8
+#bash $HOME/.conky/conky_weather/weather-pull.sh &
+#sleep 1
+#conky -c $HOME/.conky/conky_weather/conkyrc-weather &
 
 # start calendar widget
-sleep 0.8
-conky -c $HOME/.conky/conky_calendar/conkyrc-calendar &
+#sleep 0.8
+#conky -c $HOME/.conky/conky_calendar/conkyrc-calendar &
 
 # start clock widget
+#sleep 0.8
+#conky -c $HOME/.conky/conky_clock/conkyrc-clock &
+
+# start conky-rss
 sleep 0.8
-conky -c $HOME/.conky/conky_clock/conkyrc-clock &
+conky -c $HOME/.conky/conky_rss_reader/conkyrc-rss-reader &
 
 # start moc mini player
-sleep 0.8
-conky -c $HOME/.conky/conky_mini_moc_player/conkyrc-mini-moc-player &
+#sleep 0.8
+#conky -c $HOME/.conky/conky_mini_moc_player/conkyrc-mini-moc-player &
 
 # start pingbeat monitor
-sleep 0.8
-conky -c $HOME/.conky/conky_pingbeat/conkyrc-pingbeat &
+#sleep 0.8
+#conky -c $HOME/.conky/conky_pingbeat/conkyrc-pingbeat &
